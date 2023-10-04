e862031e54e9d2b35efd24d9c794ce13fed20b8b
Anupam Das <das.anupam70@gmail.com>
2023-10-04 22:35:50 +0530

    setup for heroku

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const socketio = require('socket.io')
 const { addUser,removeUser,getUser, getRoomUsers } = require('./utils/user')
 const { generateMsg } = require('./utils/message')
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000
 
 const app = express();
 const server = http.createServer(app);
